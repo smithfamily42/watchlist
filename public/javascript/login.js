2cc8eef75661cca629f7d3fcebe3c9bdc25c3da8
4FRO <drotich@ucsc.edu>
2021-06-25 15:36:10 -0700

    front end signup code

---
+++ public/javascript/login.js
@@ -0,0 +1,21 @@
+function signupFormHandler(event) {
+    event.preventDefault();
+
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    if (username && email && password) {
+        fetch('/api/users', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                email,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        }).then((response) => {console.log(response)});
+    }
+}
+
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
