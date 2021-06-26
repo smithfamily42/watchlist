7c390289caa941610f2a583fc86e5d0c072b0696
4FRO <drotich@ucsc.edu>
2021-06-25 22:25:13 -0700

    transferred routes from testServer to home-routes, now uses server.js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,15 +3,77 @@ const sequelize = require('../config/connection');
 
 // get all posts for homepage
 // users email and pw - must log in before using the site
-router.get('/', (req, res) => {
-  console.log('Breath... Encourage your teammates... Breath... Repeat...');
+
+//Mock Data until db is connected
+const movies = [
+  {
+    title: 'Transformers',
+    genre: 'action',
+    rating: 'PG-13',
+    studio: 'Paramount'
+  },
+  {
+    title: 'The Matrix',
+    genre: 'action',
+    rating: 'R',
+    studio: 'Warner Bros.'
+  },
   
-  res.render('homepage', {});
+];
+
+const shows = [
+    {
+      title: 'Lupin',
+      genre: 'action',
+      rating: 'TV-14',
+      service: 'Netflix'
+    },
+    {
+      title: 'Dave',
+      genre: 'Comedy',
+      rating: 'MA',
+      service: 'Hulu'
+    },
+    
+  ];
+
+router.get('/', (req, res) => {
+  const data = {
+    pageTitle: 'Homepage'
+  };
+  res.render('index', data);
 });
 
 
-router.get('/', (req, res) => {
+router.get('/login', (req, res) => {
+  const data = {
+    pageTitle: 'Account'
+  };
+  res.render('login', data);
+});
 
+router.get('/shows', (req, res) => {
+  const data = {
+    shows: [],
+    pageTitle: 'Shows',
+  };
+  for (let i = 0; i < shows.length; i++) { //For now shows is not defined. Needs to somehow connect it to the db
+    let currentShow = shows[i];
+    data.shows.push(currentShow);
+  };
+  res.render('shows', data);
+});
+
+router.get('/movies', (req, res) => {
+  const data = {
+    movies: [],
+    pageTitle: 'Movies',
+  };
+  for (let i = 0; i < movies.length; i++) {
+    let currentMovie = movies[i];
+    data.movies.push(currentMovie);
+  };
+  res.render('movies', data);
 });
 
 router.post('/', (req, res) => {
